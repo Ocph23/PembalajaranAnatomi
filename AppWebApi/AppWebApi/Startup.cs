9c6cc3033ebb9551f1d5be1abc51274e875f25af
ocph23 <ocph23@hotmail.com>
2017-12-28 00:40:51 +0900

    #FirstCommit

---
+++ AppWebApi/AppWebApi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AppWebApi.Startup))]
+namespace AppWebApi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
